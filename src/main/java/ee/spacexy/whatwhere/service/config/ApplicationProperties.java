162e09a7290985794e043c7dfaec86c6dd6535b9
Vladimir Domaškin <vladimir@srini.ee>
2023-11-07 20:39:53 +0200

    add config

--- src/main/java/ee/spacexy/whatwhere/service/config/ApplicationProperties.java
+++ src/main/java/ee/spacexy/whatwhere/service/config/ApplicationProperties.java
@@ -1,10 +1,49 @@
 package ee.spacexy.whatwhere.service.config;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
 @ConfigurationProperties(prefix = "application")
+@Getter
+@Setter
 public class ApplicationProperties {
 
+    public Cors cors = new Cors();
+
+    @Getter
+    public static class Cors {
+        private String[] allowedOrigins;
+        private String[] allowedMethods;
+        private String[] allowedHeaders;
+        private String[] exposedHeaders;
+        private boolean allowCredentials;
+        private long maxAge;
+
+        public void setAllowedOrigins(String[] allowedOrigins) {
+            this.allowedOrigins = allowedOrigins;
+        }
+
+        public void setAllowedMethods(String[] allowedMethods) {
+            this.allowedMethods = allowedMethods;
+        }
+
+        public void setAllowedHeaders(String[] allowedHeaders) {
+            this.allowedHeaders = allowedHeaders;
+        }
+
+        public void setExposedHeaders(String[] exposedHeaders) {
+            this.exposedHeaders = exposedHeaders;
+        }
+
+        public void setAllowCredentials(boolean allowCredentials) {
+            this.allowCredentials = allowCredentials;
+        }
+
+        public void setMaxAge(long maxAge) {
+            this.maxAge = maxAge;
+        }
+    }
 }
--- src/main/java/ee/spacexy/whatwhere/service/config/WebConfiguration.java
+++ src/main/java/ee/spacexy/whatwhere/service/config/WebConfiguration.java
@@ -1,6 +1,7 @@
 package ee.spacexy.whatwhere.service.config;
 
 import ee.spacexy.whatwhere.service.web.rest.errors.ExceptionTranslator;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -9,28 +10,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Import(ExceptionTranslator.class)
 @Configuration
+@AllArgsConstructor
 public class WebConfiguration implements WebMvcConfigurer {
-    @Value("${whatwhere.cors.allowed-origins}")
-    private String[] allowedOrigins;
-    @Value("${whatwhere.cors.allowed-methods}")
-    private String[] allowedMethods;
-    @Value("${whatwhere.cors.allowed-headers}")
-    private String[] allowedHeaders;
-    @Value("${whatwhere.cors.exposed-headers}")
-    private String[] exposedHeaders;
-    @Value("${whatwhere.cors.allow-credentials}")
-    private boolean allowCredentials;
-    @Value("${whatwhere.cors.max-age}")
-    private long maxAge;
+
+    private final ApplicationProperties applicationProperties;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
           registry.addMapping("/**")
-                .allowedOrigins(allowedOrigins)
-                .allowedMethods(allowedMethods)
-                .allowedHeaders(allowedHeaders)
-                .exposedHeaders(exposedHeaders)
-                .allowCredentials(allowCredentials)
-                .maxAge(maxAge);
+                .allowedOrigins(applicationProperties.getCors().getAllowedOrigins())
+                .allowedMethods(applicationProperties.getCors().getAllowedMethods())
+                .allowedHeaders(applicationProperties.getCors().getAllowedHeaders())
+                .exposedHeaders(applicationProperties.getCors().getExposedHeaders())
+                .allowCredentials(applicationProperties.getCors().isAllowCredentials())
+                .maxAge(applicationProperties.getCors().getMaxAge());
     }
 }
