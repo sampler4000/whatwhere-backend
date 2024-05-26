22be596ce3850542e7e584333efd76d27db134b2
Vladimir Domaškin <vladimir@srini.ee>
2024-05-26 21:45:02 +0300

    no message

---
+++ src/main/java/ee/spacexy/whatwhere/service/config/FirebaseConfigProperties.java
@@ -0,0 +1,14 @@
+package ee.spacexy.whatwhere.service.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "firebase")
+@Getter
+@Setter
+public class FirebaseConfigProperties {
+    private String config;
+}
